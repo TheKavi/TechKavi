522e5becce6476faa5906eb5bfa0315640321500
TheKavi <145855645+TheKavi@users.noreply.github.com>
2025-03-23 15:21:51 +0530

    Add files via upload

---
+++ BookNest/script.js
@@ -0,0 +1,42 @@
+// popup box , popup overlay ,button
+var popupoverlay=document.querySelector(".popup-overlay")
+var popupbox=document.querySelector(".popup-box")
+var addpopupbtn=document.querySelector(".addbook")
+
+addpopupbtn.addEventListener("click",function(){
+    popupoverlay.style.display="block";
+    popupbox.style.display="block";
+})
+
+//select cancel btn
+var cancelbtn = document.querySelector("#cancel-popup")
+
+cancelbtn.addEventListener("click",function(event){
+    event.preventDefault()
+    popupoverlay.style.display="none";
+    popupbox.style.display="none";
+})
+//select container add-book,book-title-input,book-author-input,book-author-input
+
+var container=document.querySelector(".container")
+var addbook=document.getElementById("add-book")
+var booktitleinput=document.getElementById("book-title-input")
+var bookauthorinput=document.getElementById("book-author-input")
+var bookdescriptioninput=document.getElementById("book-description-input")
+
+addbook.addEventListener("click",function(event){
+    event.preventDefault()
+    var div =document.createElement("div")
+    div.setAttribute("class","book-container")
+    div.innerHTML=`<h2>${booktitleinput.value}</h2>
+        <h5>${bookauthorinput.value}</h5>
+        <p>${bookdescriptioninput.value}</p>
+           <button onclick="deletebook(event)"> Delete</button>`
+    container.append(div)
+    popupoverlay.style.display="none";
+    popupbox.style.display="none";
+
+})
+function deletebook(event){
+    event.target.parentElement.remove()
+}
